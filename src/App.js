bbc5e6cddb2d67b978687a36977688fd511f0243
angad777 <angadraman1@gmail.com>
2019-03-13 15:02:55 +1100

    Added Apollo Client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+// Apollo Client Setup 
+const client = new ApolloClient({
+  uri: 'https://api-uswest.graphcms.com/v1/cjt6m260h2twr01ba3rno9ipc/master';
+})
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +28,8 @@ class App extends Component {
             Learn React
           </a>
         </header>
-      </div>
+        </div>
+        </ApolloProvider>
     );
   }
 }
